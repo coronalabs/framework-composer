0cda3fe7d469eeaccf03af8b958cd6969f28d666
Alexandru Frangeti <alex@coronalabs.com>
2014-05-13 16:32:28 +0200

    Fixing z-index in subsequent groups.

--- composer_scene.lua
+++ composer_scene.lua
@@ -676,17 +676,34 @@ function Scene:load( fileName )
 
 	-- then all the objects
 	local function showObjects( group, parentGroup )
-		for i = 1, #group do
-			local objData = root[ group[ i ] ]
-			local obj = self:createObject( objData )
-			parentGroup:insert( obj )
-			if obj.numChildren then
-				showObjects( objData.children, obj )
+		
+		if parentGroup ~= self.view then
+			for i = #group, 1, -1 do
+				local objData = root[ group[ i ] ]
+				local obj = self:createObject( objData )
+				parentGroup:insert( obj )
+				if obj.numChildren then
+					showObjects( objData.children, obj )
+				end
+				local tran = objData[ "timeline" ]
+				if ( #tran > 0 ) then
+					self:computeTransitions( obj, tran )
+				end
 			end
-			local tran = objData[ "timeline" ]
-			if ( #tran > 0 ) then
-				self:computeTransitions( obj, tran )
+		else
+			for i = 1, #group do
+				local objData = root[ group[ i ] ]
+				local obj = self:createObject( objData )
+				parentGroup:insert( obj )
+				if obj.numChildren then
+					showObjects( objData.children, obj )
+				end
+				local tran = objData[ "timeline" ]
+				if ( #tran > 0 ) then
+					self:computeTransitions( obj, tran )
+				end
 			end
+
 		end
 	end
 	
